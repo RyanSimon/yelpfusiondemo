ab5c7b19983f99f2d4b2375a70c90e39f0804b0d
Ryan Simon <linktothepast212@gmail.com>
2018-06-11 16:33:21 -0700

    moved search view to custom toolbar. removed menu. added support for suggestions in search

--- app/src/main/kotlin/me/ryansimon/yelpfusion/MainActivity.kt
+++ app/src/main/kotlin/me/ryansimon/yelpfusion/MainActivity.kt
@@ -2,6 +2,8 @@ package me.ryansimon.yelpfusion
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.widget.CursorAdapter
+import android.support.v4.widget.SimpleCursorAdapter
 import android.support.v7.widget.GridLayoutManager
 import android.util.Log
 import io.reactivex.Single
@@ -12,16 +14,21 @@ import me.ryansimon.yelpfusion.feature.business.*
 import me.ryansimon.yelpfusion.network.ApiConfiguration
 import me.ryansimon.yelpfusion.network.InternetConnectionHandler
 import android.support.v7.widget.RecyclerView
-import android.view.Menu
 import io.reactivex.disposables.Disposable
 import me.ryansimon.yelpfusion.feature.business.EndlessRecyclerViewScrollListener
 import android.support.v7.widget.SearchView
-import android.view.MenuItem
 import me.ryansimon.yelpfusion.extension.hideKeyboard
+import android.provider.BaseColumns
+import android.database.MatrixCursor
 
 
+/**
+ * @author Ryan Simon
+ */
 class MainActivity : AppCompatActivity() {
 
+    private val suggestions = mutableListOf<String>()
+    private val suggestionsId = "suggestions"
     private val apiConfiguration = ApiConfiguration(BuildConfig.YELP_API_KEY)
     private val businessesRepository = BusinessesRepository(
             apiConfiguration.retrofit.create(BusinessesApi::class.java),
@@ -35,34 +42,75 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
 
         setupBusinessList()
+        setupSearchView()
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main_activity, menu)
-        val menuItem = menu.findItem(R.id.action_search)
-        setupSearchView(menuItem)
-
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    private fun setupSearchView(menuItem: MenuItem) {
-        searchView = menuItem.actionView as SearchView
+    private fun setupSearchView() {
+        searchView = search_view
+        searchView.setIconifiedByDefault(false)
         searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String): Boolean {
                 val adapter = business_list_view.adapter as BusinessesAdapter
                 adapter.clear()
+
+                if(!suggestions.contains(query)) {
+                    suggestions.add(query)
+                }
+
                 performSearch(query, searchLocation, businessListView = business_list_view)
+
                 hideKeyboard()
+
                 return true
             }
 
             override fun onQueryTextChange(newText: String): Boolean {
+                populateAdapter(newText)
                 return false
             }
         })
+        searchView.setOnSuggestionListener(object : SearchView.OnSuggestionListener {
+            override fun onSuggestionClick(position: Int): Boolean {
+                val suggestion = searchView.suggestionsAdapter.getItem(position) as? String
+                suggestion?.let {
+                    performSearch(it, searchLocation, businessListView = business_list_view)
+                }
+
+                return true
+            }
+
+            override fun onSuggestionSelect(position: Int): Boolean {
+                val suggestion = searchView.suggestionsAdapter.getItem(position) as? String
+                suggestion?.let {
+                    performSearch(it, searchLocation, businessListView = business_list_view)
+                }
+
+                return true
+            }
+        })
+        val to = intArrayOf(android.R.id.text1)
+        val from = arrayOf(suggestionsId)
+        searchView.suggestionsAdapter = SimpleCursorAdapter(
+                this,
+                android.R.layout.simple_selectable_list_item,
+                null,
+                from,
+                to,
+                CursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER
+        )
         searchView.setQuery("pizza", true)
+    }
+
+    private fun populateAdapter(query: String) {
+        val matrixCursor = MatrixCursor(arrayOf(BaseColumns._ID, suggestionsId))
+        (0 until suggestions.size)
+                .filter { suggestions[it].toLowerCase().startsWith(query.toLowerCase()) }
+                .forEach { matrixCursor.addRow(arrayOf(it, suggestions[it])) }
+
+        searchView.suggestionsAdapter.changeCursor(matrixCursor)
     }
 
     private fun setupBusinessList() {
@@ -72,7 +120,7 @@ class MainActivity : AppCompatActivity() {
                 // Triggered only when new data needs to be appended to the list
                 // Add whatever code is needed to append new items to the bottom of the list
                 numResultsToSkip += numResults
-                performSearch(searchView?.query.toString(), searchLocation, numResultsToSkip, businessListView)
+                performSearch(searchView.query.toString(), searchLocation, numResultsToSkip, businessListView)
             }
         }
         businessListView.setHasFixedSize(true)
