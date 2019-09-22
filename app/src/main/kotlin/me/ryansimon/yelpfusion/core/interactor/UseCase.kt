2d422ad8a472eff98f32af3a912596332aba95b6
Ryan Simon <linktothepast212@gmail.com>
2019-09-22 15:10:41 -0700

    added flow to use case to measure request time

--- app/src/main/kotlin/me/ryansimon/yelpfusion/core/interactor/UseCase.kt
+++ app/src/main/kotlin/me/ryansimon/yelpfusion/core/interactor/UseCase.kt
@@ -1,9 +1,12 @@
 package me.ryansimon.yelpfusion.core.interactor
 
 
+import android.util.Log
 import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.*
 import me.ryansimon.yelpfusion.core.functional.Either
 import me.ryansimon.yelpfusion.core.network.Failure
+import kotlin.system.measureTimeMillis
 
 /**
  * @author https://raw.githubusercontent.com/android10/Android-CleanArchitecture-Kotlin/d5b92eb3df6e007c14a3ec4f739f4b0b2e140738/app/src/main/kotlin/com/fernandocejas/sample/core/interactor/UseCase.kt
@@ -23,11 +26,31 @@ abstract class UseCase<out Type, in Params> where Type : Any {
             coroutineScope: CoroutineScope,
             params: Params,
             onResult: (Either<Failure, Type>) -> Unit = {}) {
-        coroutineScope.launch {
-            onResult(withContext(Dispatchers.IO) {
-                run(params)
-            })
+        var measure: Long = 0
+        flow {
+            val requestTime = measureTimeMillis {
+                emit(run(params))
+            }
+
+            measure += requestTime
+
+            Log.d("Request time", "$requestTime ms")
         }
+                .flowOn(Dispatchers.IO)
+                .onStart {
+                    val delayTime = measureTimeMillis {
+                        delay(600)
+                    }
+
+                    measure += delayTime
+
+                    Log.d("Delay time", "$delayTime ms")
+                }
+                .onEach {
+                    onResult(it)
+                }
+                .onCompletion { Log.d("Flow done in", "$measure ms") }
+                .launchIn(coroutineScope)
     }
 
     class None
